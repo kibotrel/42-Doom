41f231fc9dddf7b55c2bf163d74671b2f8a67660
nde-jesu <nde-jesu@student.42.fr>
2019-10-10 16:01:02 +0200

    Changed line

--- srcs/editor/events.c
+++ srcs/editor/events.c
@@ -6,7 +6,7 @@
 /*   By: nde-jesu <nde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/13 20:22:28 by nde-jesu          #+#    #+#             */
-/*   Updated: 2019/10/09 09:07:38 by nde-jesu         ###   ########.fr       */
+/*   Updated: 2019/10/10 11:39:43 by nde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 void		print_line(t_editor *edit, int x, int y)
 {
 	t_vertex	*act_pos;
-
-	act_pos = create_vertex(x, y);
+	if (x > 0 || y > 0 || x < WIN_W || y < WIN_H)
+		act_pos = create_vertex((x / edit->dist_grid) * edit->dist_grid, (y / edit->dist_grid) * edit->dist_grid);
 	if (edit->last_vertex->x != -1 && edit->last_vertex->y != -1)
 		draw_line(edit->sdl, *edit->last_vertex, *act_pos, 0x0ff0f0);
 }
@@ -30,6 +30,7 @@ void		print_all(t_editor *edit)
 	print_ennemy(edit, 0xff0000);
 	print_object(edit, 0x00ff00);
 	print_sector(edit);
+	// printf("%i %i\n", edit->sdl->event.motion.x, edit->sdl->event.motion.y);
 	print_line(edit, edit->sdl->event.motion.x, edit->sdl->event.motion.y);
 }
 
