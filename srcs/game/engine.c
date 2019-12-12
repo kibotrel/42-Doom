ab0bd8f7eb177a431a26e43e46e2f709a339d2bc
BOTREL Kilian <botrel.kilian1999@gmail.com>
2019-12-12 05:12:52 +0100

    Normed the physics engine

--- srcs/game/engine.c
+++ srcs/game/engine.c
@@ -31,6 +31,75 @@ void	vertical_movement(t_env *env, t_sector sector, double cam_height)
 	}
 }
 
+void	find_height(t_env *env, double *hole, uint32_t s)
+{
+	int32_t		cam;
+	int32_t		neighbor;
+
+	cam = env->cam.sector;
+	neighbor = env->sector[cam].neighbor[s];
+
+	if (neighbor < 0)
+	{
+		hole[0] = 9e9;
+		hole[1] = -9e9;
+	}
+	else
+	{
+		hole[0] = max(env->sector[cam].floor, env->sector[neighbor].floor);
+		hole[1] = min(env->sector[cam].ceil, env->sector[neighbor].ceil);
+	}
+}
+
+void	vproj(t_vec3d *v, t_vec2d v1, t_vec2d v2)
+{
+	t_vec2d		w;	// Wall vector
+
+	w = v2d(v2.x - v1.x, v2.y - v1.y);
+	v->x = w.x * (v->x * w.x + w.y * v->y) / (pow(w.x, 2) + pow(w.y, 2));
+	v->y = w.y * (v->x * w.x + w.y * v->y) / (pow(w.x, 2) + pow(w.y, 2));
+}
+
+int32_t	check_collisions(t_vec3d p, t_vec3d v, t_vec2d w1, t_vec2d w2)
+{
+	if (IntersectBox(p.x, p.y, p.x + v.x, p.y + v.y, w1.x, w1.y, w2.x, w2.y))
+	{
+		if (PointSide(p.x + v.x, + p.y + v.y, w1.x, w1.y, w2.x, w2.y) < 0)
+			return (1);
+		else
+			return (0);
+	}
+	else
+		return (0);
+}
+
+void	horizontal_movement(t_env *env, t_vec3d pos, t_vec3d vel, double sight)
+{
+	uint32_t	i;
+	double		hole[2];
+	t_vec2d*	v = env->sector[env->cam.sector].vertex;
+
+	i = 0;
+	while (i < env->sector[env->cam.sector].points)
+	{
+		// Check if the player is about to cross an edge
+		if (check_collisions(pos, vel, v[i], v[i + 1]))
+		{
+			find_height(env, hole, i);
+			// Check if the height of both neighbor floor and ceil allow the player to go throw the hole
+			if (hole[0] > pos.z - sight + MARGIN_KNEE || hole[1] < pos.z + MARGIN_HEAD)
+			{
+				// vector projection to slide on the wall
+				vproj(&vel, v2d(v[i].x, v[i].y), v2d(v[i + 1].x, v[i + 1].y));
+				env->cam.move = 0;
+			}
+		}
+		i++;
+	}
+	MovePlayer(env, vel.x, vel.y);
+	env->cam.fall = 1;
+}
+
 void	physics(t_env *env)
 {
 	double sight; // State and height of the camera (standing or crouching)
@@ -42,33 +111,5 @@ void	physics(t_env *env)
 		vertical_movement(env, env->sector[env->cam.sector], sight);
 	// Horizontal collisions checks (along x and y axis)
 	if (env->cam.move)
-	{
-		float px = env->cam.pos.x;
-		float py = env->cam.pos.y;
-		float dx = env->cam.v.x;
-		float dy = env->cam.v.y;
-		const t_sector* const sect = &env->sector[env->cam.sector];
-		const t_vec2d* const vert = sect->vertex;
-		/* Check if the player is about to cross one of the sector's edges */
-		for(unsigned s = 0; s < sect->points; ++s)
-			if (IntersectBox(px , py, px + dx, py + dy, vert[s].x, vert[s].y, vert[s + 1].x, vert[s + 1].y) && PointSide(px + dx, py + dy, vert[s].x, vert[s].y, vert[s + 1].x, vert[s + 1].y) < 0)
-			{
-				/* Check where the hole is. */
-				float hole_low = sect->neighbor[s] < 0 ?  9e9 : max(sect->floor, env->sector[sect->neighbor[s]].floor);
-				float hole_high = sect->neighbor[s] < 0 ? -9e9 : min(sect->ceil, env->sector[sect->neighbor[s]].ceil);
-				/* Check whether we're bumping into a wall. */
-				if (hole_high < env->cam.pos.z + MARGIN_HEAD || hole_low > env->cam.pos.z - sight + MARGIN_KNEE)
-				{
-					/* Bumps into a wall! Slide along the wall. */
-					/* This formula is from Wikipedia article "vector projection". */
-					float xd = vert[s + 1].x - vert[s].x;
-					float yd = vert[s + 1].y - vert[s].y;
-					dx = xd * (dx * xd + yd * dy) / (xd * xd + yd * yd);
-					dy = yd * (dx * xd + yd * dy) / (xd * xd + yd * yd);
-					env->cam.move = 0;
-				}
-			}
-		MovePlayer(env, dx, dy);
-		env->cam.fall = 1;
-	}
+		horizontal_movement(env, env->cam.pos, env->cam.v, sight);
 }
