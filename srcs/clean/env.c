eb6b9d641d909d8081d7f3d5ed07f858103c3201
BOTREL Kilian <botrel.kilian1999@gmail.com>
2020-01-22 15:03:06 +0100

    Normed clean source-files

--- srcs/clean/env.c
+++ srcs/clean/env.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 19:29:45 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/11/13 06:22:32 by demonwaves       ###   ########.fr       */
+/*   Updated: 2020/01/22 15:02:39 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ static void	env_clean(t_env *env)
 
 	i = 0;
 	while (i < NB_ASSETS)
-	if (env->sdl.bmp[i++].pixels)
-		free(env->sdl.bmp[i - 1].pixels);
+		if (env->sdl.bmp[i++].pixels)
+			free(env->sdl.bmp[i - 1].pixels);
 	i = 0;
 	if (env->sector)
 	{
