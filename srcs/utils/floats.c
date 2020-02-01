2b7e1ad9ee3a0f3bb2fddf0bac3f3ae9380c864f
Lojesu <lorenzo.jesu@laposte.net>
2020-02-01 14:17:46 +0100

    little change for dtoa, now it can work on linux i think

--- srcs/utils/floats.c
+++ srcs/utils/floats.c
@@ -6,7 +6,7 @@
 /*   By: roster <roster@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 14:33:33 by roster            #+#    #+#             */
-/*   Updated: 2020/01/29 06:50:45 by kibotrel         ###   ########.fr       */
+/*   Updated: 2020/02/01 14:16:30 by lojesu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,7 +109,7 @@ char		*ft_dtoa(double d)
 	char				*str_res;
 
 	m = ft_strnew(64);
-	uli = *((unsigned long int*)(&d));
+	ft_memcpy(&uli, &d, sizeof(double));
 	i = 63;
 	while (uli)
 	{
