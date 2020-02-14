fdc2bdb1ac2af7d7d73757520c1187877d4c46b5
BOTREL Kilian <botrel.kilian1999@gmail.com>
2020-02-14 15:39:44 +0100

    Normed events

--- srcs/events/mouse.c
+++ srcs/events/mouse.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 13:51:13 by kibotrel          #+#    #+#             */
-/*   Updated: 2020/02/14 07:59:47 by kibotrel         ###   ########.fr       */
+/*   Updated: 2020/02/14 15:39:30 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	handle_mouse(t_env *env, t_sdl *sdl)
 		menu_click(env, env->data.ui, sdl->event.button.x, sdl->event.button.y);
 	else if (sdl->event.button.button == SDL_BUTTON_LEFT && env->win == EDITOR)
 		editor_click(&env->editor, sdl->event, env);
-	else if(sdl->event.type == SDL_MOUSEWHEEL && env->win == EDITOR)
+	else if (sdl->event.type == SDL_MOUSEWHEEL && env->win == EDITOR)
 		editor_mousewheel(&env->editor, sdl->event);
-	else if(sdl->event.button.button == SDL_BUTTON_LEFT && env->win == GAME)
+	else if (sdl->event.button.button == SDL_BUTTON_LEFT && env->win == GAME)
 		game_click(env, sdl->event.button.x, sdl->event.button.y);
 }
