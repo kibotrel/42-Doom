ed6d35a46b77aa6c60723b66980af0ff57a4c2a2
Lojesu <lorenzo.jesu@laposte.net>
2020-03-05 14:49:29 +0100

    delete a function

--- srcs/texture/texture_tools.c
+++ srcs/texture/texture_tools.c
@@ -6,7 +6,7 @@
 /*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 13:40:06 by kibotrel          #+#    #+#             */
-/*   Updated: 2020/02/20 14:48:40 by lojesu           ###   ########.fr       */
+/*   Updated: 2020/03/05 14:48:23 by lojesu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,6 @@
 #include "clean.h"
 #include "utils.h"
 #include "texture.h"
-
-uint32_t	color_mul(uint32_t color, double mul)
-{
-	int		r;
-	int		g;
-	int		b;
-
-	r = (color >> 16 & 0xFF);
-	g = (color >> 8 & 0xFF);
-	b = (color & 0xFF);
-	r *= mul;
-	g *= mul;
-	b *= mul;
-	r > 255 ? r = 255 : r;
-	g > 255 ? g = 255 : g;
-	b > 255 ? b = 255 : b;
-	r < 0 ? r = 0 : r;
-	g < 0 ? g = 0 : g;
-	b < 0 ? b = 0 : b;
-	return ((r << 16) | (g << 8) | b);
-}
 
 uint32_t	color_add(uint32_t color, double add)
 {
